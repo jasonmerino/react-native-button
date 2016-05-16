ae710ce9973705165cc8767a1f56a6fd3d77ef9e
Jason Merino <jasonmerino@gmail.com>
2016-05-16 13:25:02 -0700

    Updates reference to old styles.

--- index.js
+++ index.js
@@ -2,10 +2,22 @@
 
 import React, {
   PropTypes,
+  StyleSheet,
   Text,
   TouchableHighlight,
 } from 'react-native'
-import styles from '../lib/styles'
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 10,
+    height: 40,
+    borderRadius: 3,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+})
 
 export default class Button extends React.Component {
 
